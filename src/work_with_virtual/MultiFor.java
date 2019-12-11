beecc35ef3597bfa7ea6f5d6d27a586408619b58
Aleksandr  Jefimov <japhosart@gmail.com>
2019-12-11 13:10:42 +0200

    ++

--- src/work_with_virtual/MultiFor.java
+++ src/work_with_virtual/MultiFor.java
@@ -4,13 +4,16 @@ public class MultiFor {
     public static void main(String[] args) {
 
         for (int x = 0; x < 4; x++) {
-            x++;
-            if (x == 1) {
+
                 for (int y = 4; y > 2; y++) {
                     System.out.println(x + " " + y);
                 }
+                       if (x == 1) {
+                           x++;
 
-            }
+
+
+        }
 
         }
 
